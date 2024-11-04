bae389a410c5ba5b835036869b651acaa9545a05
nandigam2005246 <65825929+nandigam2005246@users.noreply.github.com>
2024-11-04 23:27:05 +0530

    [Added support for Physical Device Selection and queueFamilies]

--- Vulkan/Source/main.cpp
+++ Vulkan/Source/main.cpp
@@ -278,7 +278,10 @@ private:
 		return (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU
 			   &&
 			   deviceFeatures.geometryShader);*/
-		return true;
+
+		QueueFamilyIndices indices = findQueueFamilies(device);
+		
+		return indices.isComplete();
 	}
 
 	bool rateDeviceSuitability(VkPhysicalDevice device)
@@ -308,13 +311,40 @@ private:
 
 	struct QueueFamilyIndices
 	{
-		uint32_t graphicsFamily;
+		std::optional<uint32_t> graphicsFamily;
+
+		bool isComplete()
+		{
+			return graphicsFamily.has_value();
+		}
 	};
 
 	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
 	{
 		QueueFamilyIndices indices;
 		
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+		
+		int i = 0;
+		for (const auto& queueFamily : queueFamilies)
+		{
+			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			{
+				indices.graphicsFamily = i;
+			}
+
+			if (indices.isComplete())
+			{
+				break;
+			}
+
+			i++;
+		}
+
 		return indices;
 	}
 
